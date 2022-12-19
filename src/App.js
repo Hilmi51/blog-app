77fc23732ce61fb01d5641b1721902a3bcdd8015
Hilmi ŞAFAK <82928954+Hilmi51@users.noreply.github.com>
2022-12-19 18:43:31 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDiscord, faGithub, faInstagram, faLinkedin, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
@@ -12,24 +13,24 @@ function App() {
                     <h1>HILMI SAFAK</h1>
                     <h4><span>BACKEND DEVELOPER</span></h4>
                     <p className="icons">
-                        <a href="https://www.instagram.com/hilmi_safak/" target="_blank" className="i ai">
+                        <a href="https://www.instagram.com/hilmi_safak/" target="_blank" className="i ai" rel="noreferrer">
                             <FontAwesomeIcon icon={faInstagram} />
                         </a>
-                        <a href="https://twitter.com/Hilmi511" target="_blank" className="i ai">
+                        <a href="https://twitter.com/Hilmi511" target="_blank" className="i ai" rel="noreferrer">
                             <FontAwesomeIcon icon={faTwitter} />
                         </a>
                         <a href="https://www.linkedin.com/in/hilmi-%C5%9Fafak-40046a210/" target="_blank"
-                            className="i ai">
+                            className="i ai" rel="noreferrer">
                             <FontAwesomeIcon icon={faLinkedin} />
                         </a>
-                        <a href="https://github.com/Hilmi51" target="_blank" className="i ai">
+                        <a href="https://github.com/Hilmi51" target="_blank" className="i ai" rel="noreferrer">
                             <FontAwesomeIcon icon={faGithub} />
                         </a>
                         <a href="https://www.youtube.com/channel/UClQQsXPjHynuY4A7X18eNKw" target="_blank"
-                            className="i ai">
+                            className="i ai" rel="noreferrer">
                             <FontAwesomeIcon icon={faYoutube} />
                         </a>
-                        <a href="https://discord.gg/uJ3QJFjq5B" target="_blank" className="ai">
+                        <a href="https://discord.gg/uJ3QJFjq5B" target="_blank" className="ai" rel="noreferrer">
                             <FontAwesomeIcon icon={faDiscord} />
                         </a>
                     </p>
